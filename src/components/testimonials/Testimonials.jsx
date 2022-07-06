777639066c90896d6eed5746b0c664d8571d9eb0
NachoCarher <nachoch64@correo.ugr.es>
2022-07-06 15:26:02 +0200

    Se completa el componente Testimonials con su estilo CSS y siendo Responsive a moviles y tablets

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "John Doe",
+    review: "modi temporibus quibusdam et autem repellat provident occaecati excepturi optio reprehenderit"
+  },
+  {
+    avatar: AVTR2,
+    name: "Jane Doe",
+    review: "modi temporibus quibusdam et autem repellat provident occaecati excepturi optio reprehenderit"
+  },
+  {
+    avatar: AVTR3,
+    name: "Shawn Doe",
+    review: "modi temporibus quibusdam et autem repellat provident occaecati excepturi optio reprehenderit"
+  },
+  {
+    avatar: AVTR4,
+    name: "Michael Doe",
+    review: "modi temporibus quibusdam et autem repellat provident occaecati excepturi optio reprehenderit"
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className='testimonial' >
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
